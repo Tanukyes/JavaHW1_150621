11ef80fb397df234e72006ce5ad2310ed1cb45e8
Danila Dorozhkin <alexghostplay@gmail.com>
2021-07-02 01:28:57 +0300

    Сделал ДЗ 5

--- src/HW5/Employee.java
+++ src/HW5/Employee.java
@@ -2,21 +2,21 @@ package HW5;
 
 public class Employee {
     public static void main(String[] args) {
-        HW5.Employee vjobivatel = new HW5.Employee("Пилин Александр Иванович", "Директор", "alexd@mail.ru",
+        Employee vjobivatel = new Employee("Пилин Александр Иванович", "Директор", "alexd@mail.ru",
                 "89998744512", 100000, 43);
 
         vjobivatel.info();
 
-        HW5.Employee[] persArray = new HW5.Employee[5];
-        persArray[0] = new HW5.Employee("Иванов Иван Иванович", "Инженер", "best4best@gmail.com",
-                "87745145347", 50000, 50);
-        persArray[1] = new HW5.Employee("Дронов Артем Алексеевич", "Программист", "dron131@list.ru",
+        Employee[] persArray = new Employee[5];
+        persArray[0] = new Employee("Иванов Иван Иванович", "Инженер", "best4best@gmail.com",
+                "87745145347", 50000, 41);
+        persArray[1] = new Employee("Дронов Артем Алексеевич", "Программист", "dron131@list.ru",
                 "89013816010", 100000, 25);
-        persArray[2] = new HW5.Employee("Васильков Кирилл Викторович", "Юрист", "vasilek@mail.ru",
+        persArray[2] = new Employee("Васильков Кирилл Викторович", "Юрист", "vasilek@mail.ru",
                 "89451236441", 85000, 30);
-        persArray[3] = new HW5.Employee("Рожков Виктор Денисович", "Тракторист", "traktor4all@mailbox.com",
+        persArray[3] = new Employee("Рожков Виктор Денисович", "Тракторист", "traktor4all@mailbox.com",
                 "87485124598", 25000, 57);
-        persArray[4] = new HW5.Employee("Капетян Арман Арманович", "Кальянщик", "kaliktop@gmail.com",
+        persArray[4] = new Employee("Капетян Арман Арманович", "Кальянщик", "kaliktop@gmail.com",
                 "87548968561", 35000, 24);
 
         for (int i = 0; i < persArray.length; i++) {
@@ -24,6 +24,7 @@ public class Employee {
                 persArray[i].info();
             }
         }
+
     }
 
     private String name;
